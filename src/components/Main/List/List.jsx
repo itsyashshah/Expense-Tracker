b1850021161aaaa2f4476e0690acf6fdfcf02f3c
yash-weavik <yash@weavik.com>
2021-04-05 23:47:09 -0400

    first commit

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,43 @@
+import React, {useContext} from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import { ExpenseTrackerContext } from '../../../context/context';
+import useStyles from './styles';
+
+const List = () => {
+  const classes = useStyles();
+  const globalState = useContext(ExpenseTrackerContext);
+
+  console.log(globalState);
+//   const { transactions, deleteTransaction } = useContext(ExpenseTrackerContext);
+const transactions = [
+    {id: 1, type:"Income", category:"Salary", amount:50, date: new Date()},
+    {id: 2, type:"Expense", category:"Pets", amount:50, date: new Date()},
+    {id: 3, type:"Income", category:"Business", amount:150, date: new Date()},
+]
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick=" ">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  );
+};
+
+export default List;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
+import Form from './Form/Form'
+import List from './List/List'
+import useStyles from './styles'
+const Main = () => {
+    const classes = useStyles();
+
+    return (
+        <Card className= {classes.root}>
+            <CardHeader title="Expense Tracker"/>
+            <CardContent>
+                <Typography align="center" variant="h5"> Total Balance $100</Typography>
+                <Typography variant="subtitle1" style={{lineHeight:"1.5em", marginTop:"20px"}}> </Typography>
+                {/* InfoCard */}
+                Try Saying : Add income $100
+                <Divider />
+                <Form/>
+            </CardContent>
+            <CardContent className={classes.cartContent}>
+                <Grid container spacing={12}>
+                    <Grid item xs={12}>
+                        <List />
+
+                    </Grid>
+                </Grid>
+
+                
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
