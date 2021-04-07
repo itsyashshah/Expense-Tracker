6c663185d0f90b63ce057cef734f91f8b0dd73ce
Yash Shah <itsyashshah@gmail.com>
2021-04-07 03:18:29 -0400

    connected to speechly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {SpeechProvider} from '@speechly/react-client'
 
 import {Provider} from './context/context'
 import App from './App'
 import './index.css'
+
 ReactDOM.render(
-    <Provider>
+    <SpeechProvider appId="REACT_APP_SPEECHLY_ID" language="en-US">
+        <Provider>
          <App />
     </Provider>,
+    </SpeechProvider>,
     document.getElementById('root')
    
 )
