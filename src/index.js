e4a5e5a580661fc6ad1c8ee727a05dcb9f684cde
Yash Shah <itsyashshah@gmail.com>
2021-04-23 19:54:25 -0400

    Some refactoring

--- src/index.js
+++ src/index.js
@@ -5,13 +5,11 @@ import {SpeechProvider} from '@speechly/react-client'
 import {Provider} from './context/context'
 import App from './App'
 import './index.css'
-
 ReactDOM.render(
-    <SpeechProvider appId="9d1439ee-fc22-4a19-81bb-6e508807f5c8" language="en-US">
+    <SpeechProvider appId = {process.env.REACT_APP_SPEECHLY_ID} language="en-US">
         <Provider>
          <App />
     </Provider>,
     </SpeechProvider>,
     document.getElementById('root')
-   
 )
